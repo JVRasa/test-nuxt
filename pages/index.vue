791aa8ca293029931b9ceb90ea9fb497b98b6e11
JVRasa <j.rasamoelina@gmail.com>
2022-07-24 22:13:36 +0200

    finished css for home

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,22 @@
   <div class="h-[93vh] flex items-center">
     <section class="w-[80%] md:w-[40%] h-[80%] m-auto">
       <div class="bg-white h-[90%] p-8">
-        <h1 class="text-center text-dark-green uppercase">Lundi</h1>
-        <div class="border-b-2 border-brown text-brown">REPAS</div>
+        <h1 class="text-center text-dark-green uppercase">{{ day }}</h1>
+        <form class="flex flex-col gap-4 mt-4">
+          <label class="border-b-2 border-brown text-brown" for="meals"
+            >REPAS</label
+          >
+          <textarea id="meals" name="meals" class="bg-gray-100" rows="4" />
+          <label class="border-b-2 border-brown text-brown" for="symptoms"
+            >SYMPTOMES</label
+          >
+          <textarea
+            id="symptoms"
+            name="symptoms"
+            class="bg-gray-100"
+            rows="4"
+          />
+        </form>
       </div>
       <div class="bg-dark-green h-[10%] flex items-center justify-center">
         <button class="text-white">AJOUTER</button>
@@ -13,7 +27,24 @@
 </template>
 
 <script>
+const weekday = [
+  'Dimanche',
+  'Lundi',
+  'Mardi',
+  'Mercredi',
+  'Jeudi',
+  'Vendredi',
+  'Samedi',
+]
+const d = new Date()
+const day = weekday[d.getDay()]
+
 export default {
   name: 'IndexPage',
+  data() {
+    return {
+      day,
+    }
+  },
 }
 </script>
